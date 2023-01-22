ad26d2260a51010f4542f7d8d2e5f2ffedd75330
Maxim Ryabykh <ryabykhms@gmail.com>
2023-01-22 21:30:43 +0300

    feat: implemented mouse position command

--- src/controllers/mouse-controller/index.ts
+++ src/controllers/mouse-controller/index.ts
@@ -8,24 +8,39 @@ enum Directions {
   RIGHT = 'right',
 }
 
+enum OtherCommands {
+  POSITION = 'position',
+}
+
 export class MouseController implements IController {
-  private handlers = new Map<Directions, (y: number) => Promise<Point[]>>([
+  private directionHandlers = new Map<Directions, (y: number) => Promise<Point[]>>([
     [Directions.UP, (y: number) => up(y)],
     [Directions.DOWN, (y: number) => down(y)],
     [Directions.LEFT, (x: number) => left(x)],
     [Directions.RIGHT, (x: number) => right(x)],
   ]);
 
-  public async handle(params: ControllerParams): Promise<string> {
-    const direction = params.command.split('_')?.[1]?.trim() as Directions;
-    const handler = this.handlers.get(direction);
+  private commandHandlers = new Map<OtherCommands, () => Promise<string>>([
+    [OtherCommands.POSITION, () => this.getMousePosition()],
+  ]);
 
-    if (!handler) {
+  public async handle(params: ControllerParams): Promise<string> {
+    const commandPart = params.command.split('_')?.[1]?.trim();
+    const directionHandler = this.directionHandlers.get(commandPart as Directions);
+    const commandHandler = this.commandHandlers.get(commandPart as OtherCommands);
+
+    if (!directionHandler && !commandHandler) {
       throw new Error('Unknown mouse command');
     }
 
-    await mouse.move(handler(params.width));
+    directionHandler && (await mouse.move(directionHandler(params.width)));
+    const result = await commandHandler?.();
 
-    return params.command;
+    return result ? `${params.command} ${result}` : params.command;
+  }
+
+  private async getMousePosition(): Promise<string> {
+    const position = await mouse.getPosition();
+    return `${position.x},${position.y}`;
   }
 }
