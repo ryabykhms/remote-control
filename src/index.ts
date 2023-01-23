916d525550bde89a6031e0e051e2205f6de5d41f
Maxim Ryabykh <ryabykhms@gmail.com>
2023-01-23 18:36:45 +0300

    feat: implemented using stream for web socket data

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,32 @@
-import { WebSocketServer } from 'ws';
+import 'dotenv/config';
+import { createWebSocketStream, WebSocketServer } from 'ws';
 import { MessageController } from './controllers';
 
-const wss = new WebSocketServer({ port: 8080 });
+const port = Number.parseInt(process.env.WEB_SOCKET_PORT || '8080', 10);
+
+const webSocketServer = new WebSocketServer({ port }, () => {
+  console.log(`Web Socket Server started on PORT ${port}`);
+});
 
 const controller = new MessageController();
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', async function message(data) {
-    console.log('User sent message:', data);
-    const response = await controller.handleMessage(...data.toString().split(' '));
-    ws.send(response);
+webSocketServer.on('connection', (ws) => {
+  const webSocketStream = createWebSocketStream(ws, {
+    decodeStrings: false,
+    encoding: 'utf-8',
   });
+
+  webSocketStream.on('data', async (data: string) => {
+    console.log('User sent message:', data);
+    const response = await controller.handleMessage(...data.split(' '));
+    webSocketStream.write(response);
+  });
+});
+
+webSocketServer.on('close', () => {
+  console.log('WebSocket connection was closed');
+});
+
+webSocketServer.on('error', (err) => {
+  console.error(err);
 });
