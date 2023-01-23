86163fa8e93e3b8fd879066a6f537e527b2180fc
Maxim Ryabykh <ryabykhms@gmail.com>
2023-01-23 19:01:26 +0300

    feat: added http server to project and moved the ws server to a separate class

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,11 @@
 import 'dotenv/config';
-import { createWebSocketStream, WebSocketServer } from 'ws';
-import { MessageController } from './controllers';
 
-const port = Number.parseInt(process.env.WEB_SOCKET_PORT || '8080', 10);
+import { httpServer } from './http-server';
+import { wsServer } from './ws-server';
 
-const webSocketServer = new WebSocketServer({ port }, () => {
-  console.log(`Web Socket Server started on PORT ${port}`);
-});
+const httpPort = Number.parseInt(process.env.HTTP_PORT || '8181', 10);
+const wsPort = Number.parseInt(process.env.WEB_SOCKET_PORT || '8080', 10);
 
-const controller = new MessageController();
-
-webSocketServer.on('connection', (ws) => {
-  const webSocketStream = createWebSocketStream(ws, {
-    decodeStrings: false,
-    encoding: 'utf-8',
-  });
-
-  webSocketStream.on('data', async (data: string) => {
-    console.log('User sent message:', data);
-    const response = await controller.handleMessage(...data.split(' '));
-    webSocketStream.write(response);
-  });
-});
-
-webSocketServer.on('close', () => {
-  console.log('WebSocket connection was closed');
-});
-
-webSocketServer.on('error', (err) => {
-  console.error(err);
-});
+console.log(`Start static http server on the ${httpPort} port!`);
+httpServer.listen(httpPort);
+wsServer.listen(wsPort);
